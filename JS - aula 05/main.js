becf9875d45dd0336691cc30643e69299c586b93
Helloisa22 <helloisa.betriz44@gmail.com>
2022-05-10 13:26:19 -0300

    Adicionando testes em JS

---
+++ JS - aula 05/main.js
@@ -0,0 +1,11 @@
+let string1 = "Dr Stephen Strange lança um feitiço proibido que abre um portal para o multiverso. No entanto, surge uma ameaça que pode ser grande demais para sua equipe lidar."
+
+// string2 = string1.replace('Dr Stephen Strange', 'Feiticeira Scarlate')
+
+// console.log(string1.replace('Dr Stephen Strange', 'Feiticeira Scarlate'))
+// string2 = string1.split(' ')
+// console.log(string2)
+// console.log(string2[13])
+
+// console.log(string1.toUpperCase('Dr Stephen Strange'))
+
