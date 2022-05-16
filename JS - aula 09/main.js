bed9f955273a0fffdb52840b7e87dc1b8c090de8
Helloisa22 <helloisa.betriz44@gmail.com>
2022-05-15 23:35:27 -0300

    Add metodos de JS com arrays

---
+++ JS - aula 09/main.js
@@ -0,0 +1,53 @@
+// const frutas = ['apple', 'orange', 'cherry'];
+// frutas.forEach(minhaFuncao)
+
+//Função no formato extenso
+// function minhaFuncao(item, pera){
+//     console.log(`Indice: ${pera}`)
+//     console.log(`Elemento: ${item}`);
+// }
+
+//ForEach com Arrow Function
+// const frutas01 = ['apple', 'orange', 'cherry'];
+// frutas01.forEach((index, item1) =>{
+//     console.log(`Indice: ${index}`);
+//     console.log(`Elemento: ${item1}`)
+// })
+
+// Arrays de Objetos
+const tarefas = [
+    {
+        id: 1,
+        texto: 'Levar o lixo para fora',
+        isCompleted: true,
+    },
+    {
+        id: 2,
+        texto: 'Encontrar com o chefe',
+        isCompleted: true,
+    },
+    {
+        id: 3,
+        texto: 'Consulta no dentista',
+        isCompleted: false,
+    },
+];
+
+// tarefas.forEach((teste) => {console.log(teste.texto), console.log(teste.id)})
+
+//Método map()
+// const numeros = [4, 9, 16, 25];
+// const newArray = numeros.map(Math.sqrt);
+
+// console.log('Números:');
+// console.log(numeros);
+// console.log('Novo array:');
+// console.log(newArray);
+
+// const mapText = tarefas.map((valor) => {
+//     return valor.texto;
+// })
+
+// console.log(mapText);
+
+// Método filter()
