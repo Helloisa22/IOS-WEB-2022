be702e9b430c3ccca5765c58e1ea6414b2abc6a9
Helloisa22 <helloisa.betriz44@gmail.com>
2022-05-18 09:15:34 -0300

    Atividades de DOM p2

---
+++ JS - 12 - DOM P2/mian.js
@@ -0,0 +1,14 @@
+// metodo getElementById
+// get      Element     By      Id
+// pegar    Elemento    Pelo    ID
+
+let titulo = document.getElementById('titulo');
+
+titulo.innerHTML = 'Olá meus caros alunos!';
+
+// configurando o estilo CSS do elemento
+titulo.style.textAlign = 'center';
+titulo.style.backgroundColor = '#CCCCC9';
+titulo.style.borderBottom = 'solid 3px #000';
+titulo.style.margin = '20px';
+
